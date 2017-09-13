82a2283ec8de14e3aa7e976c96af9676414cc38e
ash <ash@strategicdata.com.au>
2017-09-13 13:46:10 +1000

    Update dependencies

--- resources/leiningen/new/compojure_rest_base/project.clj
+++ resources/leiningen/new/compojure_rest_base/project.clj
@@ -1,11 +1,11 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "Compojure rest application"
   :url "http://github.com/amurf/compojure-rest-base"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-json "0.3.1"]]
-  :plugins [[lein-ring "0.8.11"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.6.0"]
+                 [ring/ring-json "0.4.0"]]
+  :plugins [[lein-ring "0.12.1"]]
   :ring {:handler {{name}}.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring/ring-mock "0.3.1"]]}})
